70c334290f159b337222d28e077e482e581623cc
adam-103009 <20120s49@gmail.com>
2022-11-21 15:05:57 +0800

    first

---
+++ knn.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Knn Regression 
+class myKnnLinearRegression():
+    def __init__(self):
+        pass
+    def train(self):
+        pass
+    def predict(self):
+        pass
+# 讀取資料
+data = np.load('data/data1.npz')
